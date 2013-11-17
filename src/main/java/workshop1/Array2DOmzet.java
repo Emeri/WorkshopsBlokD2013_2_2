94081cfb14ca88aeb63cef6939a01c6636629567
Remco Ruijsenaars <remcoruijsenaars@192.168.2.2>
2013-11-17 11:54:22 +0100

    First commit

---
+++ src/main/java/workshop1/Array2DOmzet.java
@@ -0,0 +1,78 @@
+package workshop1;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: remcoruijsenaars
+ * Date: 11/16/13
+ * Time: 4:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Array2DOmzet {
+
+    public static void main(String[] args){
+        double[][] omzetCijfers = {
+                                    {1220, 2345, 6700, 3000}, //huidig jaar
+                                    {5300, 2000, 1000, 5500},
+                                    {2316, 6244, 2445, 6255},
+                                    {4225, 1425, 2677, 6777},
+                                    {4524, 3616, 4352, 5344},
+                                    {3452, 25377, 5200, 7000}
+                                };
+
+
+
+        int jaar = meesteOmzetInJaar(omzetCijfers);
+        System.out.println("Meeste omzet in jaar: " + jaar);
+
+        int kwartaal = meesteOmzetInKwartaal(omzetCijfers);
+        System.out.println("Meeste omzet in kwartaal: " + kwartaal);
+    }
+
+
+    private static int meesteOmzetInKwartaal(double[][] input){
+
+        final int JAREN = input.length;
+        final int KWARTALEN = input[0].length;
+
+        double maxOmzetPerKwartaal = Integer.MIN_VALUE;
+        int indexKwartaal = 0;
+
+        for(int j = 0; j<KWARTALEN;j++){
+            double omzetKwartaal = 0;
+            for(int i=0; i<JAREN; i++){
+                omzetKwartaal+=input[i][j];
+            }
+            if(omzetKwartaal > maxOmzetPerKwartaal){
+                maxOmzetPerKwartaal = omzetKwartaal;
+                indexKwartaal = j;
+            }
+        }
+
+        return indexKwartaal + 1;
+    }
+
+
+    private static int meesteOmzetInJaar(double[][] input){
+
+        int huidigJaar = Calendar.getInstance().get(Calendar.YEAR);
+
+        double maxOmzetPerJaar = Integer.MIN_VALUE;
+        int indexJaar = 0;
+        for(int i=0; i<input.length; i++){
+            double omzetIter = 0;
+            for(int j=0; j<input[i].length; j++){
+                omzetIter+= input[i][j];
+            }
+            if(omzetIter > maxOmzetPerJaar){
+                indexJaar = i;
+                maxOmzetPerJaar = omzetIter;
+            }
+        }
+
+        return huidigJaar - indexJaar;
+
+    }
+}
---
+++ src/main/java/workshop1/Array2DOmzetStart.java
@@ -0,0 +1,53 @@
+package workshop1;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: remcoruijsenaars
+ * Date: 11/16/13
+ * Time: 4:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Array2DOmzetStart {
+
+    public static void main(String[] args){
+        double[][] omzetCijfers = {
+                                    {1220, 2345, 6700, 3000}, //huidig jaar
+                                    {5300, 2000, 1000, 5500},
+                                    {2316, 6244, 2445, 6255},
+                                    {4225, 1425, 2677, 6777},
+                                    {4524, 3616, 4352, 5344},
+                                    {3452, 25377, 5200, 7000}
+                                };
+
+
+
+        ArrayList<String> namen = new ArrayList();
+        namen.add("Mees");
+        namen.add("Guus");
+        namen.add(1, "Thom");
+        namen.remove(0);
+
+
+
+        int jaar = meesteOmzetInJaar(omzetCijfers);
+        System.out.println("Meeste omzet in jaar: " + jaar);
+
+        int kwartaal = meesteOmzetInKwartaal(omzetCijfers);
+        System.out.println("Meeste omzet in kwartaal: " + kwartaal);
+    }
+
+
+    //TODO implement
+    private static int meesteOmzetInKwartaal(double[][] input){
+         return 0;
+
+    }
+
+    //TODO implement
+    private static int meesteOmzetInJaar(double[][] input){
+         return 0;
+    }
+}
