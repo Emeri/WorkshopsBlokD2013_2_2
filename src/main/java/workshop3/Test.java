30c0527f1ee97d4be3c73b9f8e73d7722b1eb2fe
remcoruijsenaars <rruijsen@hhs.nl>
2013-11-19 19:02:22 +0100

    Test added

---
+++ src/main/java/workshop3/Test.java
@@ -0,0 +1,11 @@
+package workshop3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: remcoruijsenaars
+ * Date: 11/19/13
+ * Time: 7:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
