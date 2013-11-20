f600810a09a17c26aa9fac2f1098ad3412180e0d
remcoruijsenaars <rruijsen@hhs.nl>
2013-11-20 10:29:34 +0100

    Test added

---
+++ src/main/java/workshop2/Test.java
@@ -0,0 +1,11 @@
+package workshop2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: remcoruijsenaars
+ * Date: 11/20/13
+ * Time: 10:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
